7037828910763f6fe60af2c45e1eab2d703b7c02
chatchai.s <chatchai.s@goldenland.co.th>
2019-08-19 09:07:21 +0700

    Created - initial projecte

---
+++ FooBarQix/C7/FooBarQix.cs
@@ -0,0 +1,71 @@
+﻿namespace C7
+{
+    using System;
+
+    internal class FooBarQix
+    {
+        public FooBarQix()
+        {
+        }
+
+        internal object Compute(int number)
+        {
+            string result = CheckDivisible(number);
+            result = CheckContain(number, result);
+
+            if (string.IsNullOrEmpty(result))
+            {
+                result = number.ToString();
+            }
+
+            return result;
+        }
+
+        private static string CheckContain(int number, string result)
+        {
+            foreach (var item in number.ToString())
+            {
+                if (item == '3')
+                {
+                    result += "Foo";
+                }
+                else if (item == '5')
+                {
+                    result += "Bar";
+                }
+                else if (item == '7')
+                {
+                    result += "Qix";
+                }
+            }
+
+            return result;
+        }
+
+        private static string CheckDivisible(int number)
+        {
+            var result = string.Empty;
+
+            var isDivisibleBy3 = number % 3 == 0;
+            var isDivisibleBy5 = number % 5 == 0;
+            var isDivisibleBy7 = number % 7 == 0;
+
+            if (isDivisibleBy3)
+            {
+                result += "Foo";
+            }
+
+            if (isDivisibleBy5)
+            {
+                result += "Bar";
+            }
+
+            if (isDivisibleBy7)
+            {
+                result += "Qix";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FooBarQix/C7/FooBarQixTest.cs
@@ -0,0 +1,60 @@
+﻿namespace C7
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Xunit;
+
+    public class FooBarQixTest
+    {
+        private FooBarQix _sut;
+
+        public FooBarQixTest()
+        {
+            this._sut = new FooBarQix();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        public void Compute_NotDivisibleBy3Or5Or7_ReturnNumber(int number)
+        {
+            var expected = number.ToString();
+            var actual = this._sut.Compute(number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(6, "Foo")]
+        [InlineData(10, "Bar")]
+        [InlineData(14, "Qix")]
+        public void Compute_DivisibleBy3Or5Or7_ReturnExpected(int number, string expected)
+        {
+            var actual = this._sut.Compute(number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(3, "FooFoo")]
+        [InlineData(5, "BarBar")]
+        [InlineData(7, "QixQix")]
+        public void Compute_Contain3Or5Or7_ReturnExpected(int number, string expected)
+        {
+            var actual = this._sut.Compute(number);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(15, "FooBarBar")]
+        [InlineData(33, "FooFooFoo")]
+        [InlineData(21, "FooQix")]
+        [InlineData(51, "FooBar")]
+        [InlineData(53, "BarFoo")]
+        public void Compute_MultipleCondition_ReturnExpected(int number, string expected)
+        {
+            var actual = this._sut.Compute(number);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
